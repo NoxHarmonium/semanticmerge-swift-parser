198cb0c355978ef98b94102f4760ecb9f50ecd9f
Sean Dawson <sean.dawson@outware.com.au>
2016-12-20 21:47:31 +1100

    Add YamlSwift dependency

--- Package.swift
+++ Package.swift
@@ -4,5 +4,6 @@ let package = Package(
     name: "semanticmerge-swift-parser",
      dependencies: [
         .Package(url: "https://github.com/jpsim/SourceKitten.git", majorVersion: 0, minor: 16),
+        .Package(url: "git@github.com:behrang/YamlSwift.git", majorVersion: 3, minor: 3),
     ]
 )
