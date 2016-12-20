58e589d85f8499dfb0a32b64ce847e41b3a46fbc
Sean Dawson <sean.dawson@outware.com.au>
2016-12-20 21:59:43 +1100

    Remove SwiftYaml dependency because it can't serialise

--- Package.swift
+++ Package.swift
@@ -4,6 +4,5 @@ let package = Package(
     name: "semanticmerge-swift-parser",
      dependencies: [
         .Package(url: "https://github.com/jpsim/SourceKitten.git", majorVersion: 0, minor: 16),
-        .Package(url: "git@github.com:behrang/YamlSwift.git", majorVersion: 3, minor: 3),
     ]
 )
