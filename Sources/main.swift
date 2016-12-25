27e2b53fdf3097176d43e14cc30ac1f387a0db57
Sean Dawson <sean.dawson@outware.com.au>
2016-12-25 20:19:37 +1100

    Correct stdin handling to SemanticeMerge spec

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,10 +1,56 @@
-var buffer: String = ""
+#if os(Linux)
+  import Glibc
+#else
+  import Darwin.C
+#endif
+import Dispatch
 
-for line in readFromStdin() {
-  if (line == "end\n") {
-    let output = processBuffer(buffer: buffer)
-    print ("---\n\(output.toYamlString())")
-    break
+func printHelp(_ args: [String]) {
+  print("Usage: \(args[0]) shell flag_file_path")
+  print("See: https://users.semanticmerge.com/documentation/external-parsers/external-parsers-guide.shtml")
+}
+
+func argumentsAreValid(_ args: [String]) -> Bool {
+  return args.count == 3 && args[1] == "shell"
+}
+
+func createFlagFile(_ flagFile: String) {
+  try! "\n".write(toFile: flagFile, atomically: false, encoding: String.Encoding.utf8)
+}
+
+func exitIfEnd(_ value: String) {
+  if value.trimmingCharacters(in: .whitespacesAndNewlines) == "end" {
+    exit(0)
   }
-  buffer += line
+}
+
+
+let args = CommandLine.arguments
+if !argumentsAreValid(args) {
+  printHelp(args)
+  exit(1)
+}
+
+let flagFile = args[2]
+
+createFlagFile(flagFile)
+
+let iter = readFromStdin()
+var lastValue: String? = nil
+
+for value in iter {
+  exitIfEnd(value)
+  
+  guard let inputFile = lastValue?.trimmingCharacters(in: .whitespacesAndNewlines) else { 
+    lastValue = value
+    continue
+  }
+
+  let outputFile = value.trimmingCharacters(in: .whitespacesAndNewlines)
+
+  let buffer = try! String(contentsOfFile: inputFile, encoding: String.Encoding.utf8)
+  let outputObject = processBuffer(buffer: buffer)
+  let outputString = outputObject.toYamlString()
+  print ("---\n\(outputString)")
+  try! outputString.write(toFile: outputFile, atomically: false, encoding: String.Encoding.utf8)
 }
